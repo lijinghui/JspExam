7bd6be6b1d53fa8a5a00acd90682d914e688d35c
lijinghui <769512349@qq.com>
2015-08-16 20:36:15 +0800

    chang del

--- src/main/java/com/servlet/DelServlet.java
+++ src/main/java/com/servlet/DelServlet.java
@@ -18,43 +18,62 @@ import com.hand.util.ConnectionFactory;
  */
 public class DelServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DelServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public DelServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Connection conn = ConnectionFactory.getInstance().makeConnextion();
+		System.out.println(request.getParameter("id"));
+		int a = Integer.parseInt(request.getParameter("id").toString());
+
+		String sql = "delete from film_actor where film_id=?";
+		PreparedStatement preparedStatement;
 		try {
-			PreparedStatement ps = conn.prepareStatement("DELETE  FROM film WHERE film_id = ?");
-			System.out.println(request.getParameter("id"));
-			int a = Integer.parseInt(request.getParameter("id").toString());
-			ps.setInt(1,a );
-			System.out.println(ps.toString());
-			ps.execute();
-			
+			preparedStatement = conn.prepareCall(sql);
+			preparedStatement.setInt(1, a);
+			preparedStatement.execute();
+			sql = "delete from film_category where film_id=?";
+			preparedStatement = conn.prepareCall(sql);
+			preparedStatement.setInt(1, a);
+			preparedStatement.execute();
+			sql = "delete from rental where inventory_id in(select inventory_id from inventory where film_id=?)";
+			preparedStatement = conn.prepareCall(sql);
+			preparedStatement.setInt(1, a);
+			preparedStatement.execute();
+			sql = "delete from inventory where film_id=?";
+			preparedStatement = conn.prepareCall(sql);
+			preparedStatement.setInt(1, a);
+			preparedStatement.execute();
+			sql = "delete from film where film_id=?";
+			preparedStatement = conn.prepareCall(sql);
+			preparedStatement.setInt(1, a);
+			preparedStatement.execute();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("INFO-->删除出错！");
 		}
-		
-		RequestDispatcher rd = request.getRequestDispatcher("film.jsp");
-		rd.forward(request, response);
-	}
 
+	}
 }
